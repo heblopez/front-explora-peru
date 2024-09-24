f5df41162fd35fbdec6464c8799a62bfb9de3684
Heberth López <heberth.lopez.19@gmail.com>
2024-09-23 22:49:37 -0500

    chore: :tada: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Explora Perú</h1>
+      <p>Hello Vite + React!</p>
+    </>
+  )
+}
+
+export default App

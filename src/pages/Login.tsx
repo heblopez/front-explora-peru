a3a3391c711a04545101b7562dd2e5bf3cd52ca8
Heberth López <heberth.lopez.19@gmail.com>
2024-09-25 13:50:24 -0500

    feat: :construction: Added Login Page interface and added new colors in Tailwind config
    Added first login interface using ShadCN ui components and added new colors in tailwind.config.js

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardHeader,
@@ -6,24 +7,74 @@ import {
   CardTitle,
   CardFooter
 } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Link } from 'react-router-dom'
 
-function Login() {
+export default function Login() {
   return (
-    <main className='flex justify-center'>
-      <Card>
+    <main className='flex justify-center p-4'>
+      <Card className='w-full max-w-md'>
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle className='text-2xl font-bold text-center text-primary'>
+            Iniciar Sesión
+          </CardTitle>
+          <CardDescription className='text-center'>
+            Accede a tu cuenta con tus credenciales
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <form>
+            <div className='space-y-4'>
+              <div className='space-y-2'>
+                <label
+                  htmlFor='email'
+                  className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                >
+                  Correo Electrónico:
+                </label>
+                <Input
+                  id='email'
+                  type='email'
+                  placeholder='mail@example.com'
+                  required
+                />
+              </div>
+              <div className='space-y-2'>
+                <label
+                  htmlFor='password'
+                  className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                >
+                  Contraseña:
+                </label>
+                <Input id='password' type='password' required />
+              </div>
+            </div>
+          </form>
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
+        <CardFooter className='flex flex-col space-y-4'>
+          <Button className='w-full bg-slate-700 hover:bg-primary-darker'>
+            Iniciar Sesión
+          </Button>
+          <div className='text-sm text-center'>
+            ¿Olvidaste tu contraseña? <br />
+            <Link
+              to='#'
+              className='text-primary-dark font-bold hover:underline'
+            >
+              Recupérala aquí
+            </Link>
+          </div>
+          <div className='text-sm text-center'>
+            ¿No tienes una cuenta? <br />
+            <Link
+              to='/register'
+              className='text-primary-dark font-bold hover:underline'
+            >
+              Regístrate
+            </Link>
+          </div>
         </CardFooter>
       </Card>
     </main>
   )
 }
-
-export default Login

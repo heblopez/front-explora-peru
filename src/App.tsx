e5085215bdb71c24dda12bda9dd6ebab38018c24
Heberth López <heberth.lopez.19@gmail.com>
2024-09-26 22:37:13 -0500

    chore: :adhesive_bandage: Update route for Agencies landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Agencies from './pages/Agencies'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Tours from './pages/Tours'
@@ -21,9 +20,8 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/AgenciesLanding' element={<AgenciesLanding />} />
           <Route path='/' element={<Home />} />
-          <Route path='/agencies' element={<Agencies />} />
+          <Route path='/agencies' element={<AgenciesLanding />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/tours' element={<Tours />} />

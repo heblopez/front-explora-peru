a6dee570e52cb529fdaf0e97c4edf3be33235f9d
Heberth López <heberth.lopez.19@gmail.com>
2024-10-02 22:50:58 -0500

    feat: :sparkles: Implemented fake API with json-server, and fake login

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -34,7 +34,7 @@ export default function Register() {
   }
 
   return (
-    <main className='flex justify-center my-auto p-4'>
+    <main className='flex justify-center my-auto px-4 py-8'>
       <Card className='w-full max-w-md'>
         <CardHeader>
           <CardTitle className='text-3xl font-bold text-center text-primary dark:text-primary-lighter'>
